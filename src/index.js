74e7269d0b9ddd6ce2d09859c51bf02bd6de902c
Afgalopez <afgalopez@gmail.com>
2023-12-04 16:06:57 -0500

    HW done

--- src/index.js
+++ src/index.js
@@ -1,65 +1,173 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
-import './styles.css';
+import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const ref = {
-  selector: document.querySelector('.breed-select'),
-  divCatInfo: document.querySelector('.cat-info'),
-  loader: document.querySelector('.loader'),
-  error: document.querySelector('.error'),
+export const elements = {
+  galleryEl: document.querySelector('.gallery'),
+  searchInput: document.querySelector('input[name="searchQuery"]'),
+  searchForm: document.getElementById('search-form'),
+  loaderEl: document.querySelector('.loader'),
+  loadMoreBtn: document.querySelector('.load-more'),
 };
-const { selector, divCatInfo, loader, error } = ref;
 
-loader.classList.replace('loader', 'is-hidden');
-error.classList.add('is-hidden');
-divCatInfo.classList.add('is-hidden');
+export const BASE_URL = 'https://pixabay.com/api/';
+export const API_KEY = '40998850-21238c0a5b68611eff0d55619';
+export const options = {
+  params: {
+    key: API_KEY,
+    q: '',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 40,
+    page: 1,
+  },
+};
 
-let arrBreedsId = [];
-fetchBreeds()
-  .then(data => {
-    data.forEach(element => {
-      arrBreedsId.push({ text: element.name, value: element.id });
-    });
-    new SlimSelect({
-      select: selector,
-      data: arrBreedsId,
-    });
-  })
-  .catch(onFetchError);
+const { galleryEl, searchInput, searchForm, loaderEl } = elements;
 
-selector.addEventListener('change', onSelectBreed);
+let totalHits = 0;
+let isLoadingMore = false;
+let reachedEnd = false;
 
-function onSelectBreed(event) {
-  loader.classList.replace('is-hidden', 'loader');
-  selector.classList.add('is-hidden');
-  divCatInfo.classList.add('is-hidden');
+const lightbox = new SimpleLightbox('.lightbox', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  enableKeyboard: true,
+  showCounter: false,
+  scrollZoom: false,
+  close: false,
+});
 
-  const breedId = event.currentTarget.value;
-  fetchCatByBreed(breedId)
-    .then(data => {
-      loader.classList.replace('loader', 'is-hidden');
-      selector.classList.remove('is-hidden');
-      const { url, breeds } = data[0];
+searchForm.addEventListener('submit', onFormSybmit);
+window.addEventListener('scroll', onScrollHandler);
+document.addEventListener('DOMContentLoaded', hideLoader);
 
-      divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div><div class="box"><h1>${breeds[0].name}</h1><p>${breeds[0].description}</p><p><b>Temperament:</b> ${breeds[0].temperament}</p></div>`;
-      divCatInfo.classList.remove('is-hidden');
-    })
-    .catch(onFetchError);
+function showLoader() {
+  loaderEl.style.display = 'block';
 }
 
-function onFetchError(error) {
-  selector.classList.remove('is-hidden');
-  loader.classList.replace('loader', 'is-hidden');
+function hideLoader() {
+  loaderEl.style.display = 'none';
+}
 
-  Notify.failure(
-    'Oops! Something went wrong! Try reloading the page or select another cat breed!',
-    {
-      position: 'center-center',
-      timeout: 5000,
-      width: '400px',
-      fontSize: '24px',
+function renderGallery(hits) {
+  const markup = hits
+    .map(item => {
+      return `
+            <a href="${item.largeImageURL}" class="lightbox">
+                <div class="photo-card">
+                    <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes</b>
+                            ${item.likes}
+                        </p>
+                        <p class="info-item">
+                            <b>Views</b>
+                            ${item.views}
+                        </p>
+                        <p class="info-item">
+                            <b>Comments</b>
+                            ${item.comments}
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads</b>
+                            ${item.downloads}
+                        </p>
+                    </div>
+                </div>
+            </a>
+            `;
+    })
+    .join('');
+
+  galleryEl.insertAdjacentHTML('beforeend', markup);
+
+  
+
+  if (options.params.page * options.params.per_page >= totalHits) {
+    if (!reachedEnd) {
+      Notify.info("We're sorry, but you've reached the end of search results.");
+      reachedEnd = true;
     }
-  );
+  }
+  lightbox.refresh();
+  if (options.params.page === 1) {
+    showLoadMoreButton();
+  }
+}
+
+function showLoadMoreButton() {
+  elements.loadMoreBtn.style.display = 'block';
+}
+
+async function loadMore() {
+  isLoadingMore = true;
+  options.params.page += 1;
+  try {
+    showLoader();
+    const response = await axios.get(BASE_URL, options);
+    const hits = response.data.hits;
+    renderGallery(hits);
+  } catch (err) {
+    Notify.failure(err);
+    hideLoader();
+  } finally {
+    hideLoader();
+    isLoadingMore = false;
+  }
+}
+
+function onScrollHandler() {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+  const scrollThreshold = 300;
+  if (
+    scrollTop + clientHeight >= scrollHeight - scrollThreshold &&
+    galleryEl.innerHTML !== '' &&
+    !isLoadingMore &&
+    !reachedEnd
+  ) {
+
+  }
+}
+
+elements.loadMoreBtn.addEventListener('click', loadMoreOnClick);
+
+function loadMoreOnClick() {
+  if (!isLoadingMore && !reachedEnd) {
+    loadMore();
+  }
+}
+
+async function onFormSybmit(e) {
+  e.preventDefault();
+  options.params.q = searchInput.value.trim();
+  if (options.params.q === '') {
+    return;
+  }
+  options.params.page = 1;
+  galleryEl.innerHTML = '';
+  reachedEnd = false;
+
+  try {
+    showLoader();
+    const response = await axios.get(BASE_URL, options);
+    totalHits = response.data.totalHits;
+    const hits = response.data.hits;
+    if (hits.length === 0) {
+      Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+      renderGallery(hits);
+    }
+    searchInput.value = '';
+    hideLoader();
+  } catch (err) {
+    Notify.failure(err);
+    hideLoader();
+  }
 }
